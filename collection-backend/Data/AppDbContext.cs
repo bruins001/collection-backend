05fe1a4db2226ac81d4592d22cc9323d0f967503
bruins001 <niels.bruinsma2002@gmail.com>
2024-10-02 18:38:27 +0200

    Add in memory database

---
+++ collection-backend/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using collection_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace collection_backend.Data
+{
+    public class AppDbContext: DbContext
+    {
+        DbSet<Tool> Tools;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    }
+}
--- collection-backend/Program.cs
+++ collection-backend/Program.cs
@@ -1,4 +1,7 @@
 
+using collection_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace collection_backend
 {
     public class Program
@@ -13,6 +16,9 @@ namespace collection_backend
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("app")
+            );
 
             var app = builder.Build();
 
