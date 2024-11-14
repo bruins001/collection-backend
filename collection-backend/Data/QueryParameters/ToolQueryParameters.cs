c271e187ab54cbfe7d241bd217c64475b4f8171e
Niels Bruinsma <62013574+bruins001@users.noreply.github.com>
2024-11-14 19:52:52 +0100

    Update ToolQueryParameters.cs

--- collection-backend/Data/QueryParameters/ToolQueryParameters.cs
+++ collection-backend/Data/QueryParameters/ToolQueryParameters.cs
@@ -1,21 +1,12 @@
 ﻿namespace collection_backend.Data.QueryParameters;
 
-public class ToolQueryParameters: QueryParameters
+public class ToolQueryParameters
 {
-    // Hide properties
-    public bool HideId { get; set; } = true;
-    public bool HideName { get; set; } = true;
-    public bool HideDescription { get; set; } = true;
-    public bool HideType { get; set; } = true;
-    public bool HideElectric { get; set; } = true;
-    public bool HideProductCode { get; set; } = true;
-    public bool HideEan { get; set; } = true;
-    
     // Custom properties
     public int? Id { get; set; } = null;
     public string? Name { get; set; } = null;
     public string? Description { get; set; } = null;
-    public int? Type { get; set; } = null;
+    public string? Type { get; set; } = null;
     public string? ProductCode { get; set; } = null;
     public string? Ean { get; set; } = null;
 }
